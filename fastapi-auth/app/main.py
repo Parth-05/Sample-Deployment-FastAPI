d8ec41e5484a8b35cf246359e6b6f64844b3c442
Parth-05 <parthmarathe5@gmail.com>
2025-09-10 11:46:55 -0700

    Added root route

--- fastapi-auth/app/main.py
+++ fastapi-auth/app/main.py
@@ -26,6 +26,10 @@ async def on_startup():
 
 app.include_router(auth_router.router, prefix="/api/v1/auth")
 
+@app.get("/")
+async def root():
+    return {"status": "server healthy"}
+
 @app.get("/health")
 async def health():
     return {"status": "ok"}
